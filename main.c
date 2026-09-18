422a75fefdbb007b37ab5c6b317c1bab918d0156
~EM <emanuelpedroadaodasilva81@gmail.com>
2026-09-17 21:43:01 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char nome {20};
+    int idade;
+    int anos;
+    int MAIORIDADE;
+
+    printf("Olá! Qual o seu nome?!\n");
+    scanf ("%s, nome");
+    printf("Nome: %s\n", nome);
+
+    return 0;
+}
